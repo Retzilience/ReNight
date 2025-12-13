621a55ef56817071b8b51a59d19a5f2f919e556c
Retzilience <94235865+Retzilience@users.noreply.github.com>
2025-12-12 21:06:50 -0300

    Update renight_utils.py
    0.3

--- renight_utils.py
+++ renight_utils.py
@@ -1,5 +1,6 @@
 # renight_utils.py
-# External URL opener with Linux, so when binaries are compiled they don't bug out with KDE opening Qt links. ¯\_(ツ)_/¯ 
+# External URL opener with Linux, so when binaries are compiled they 
+#     don't bug out with KDE opening Qt links. ¯\_(ツ)_/¯ 
 
 from __future__ import annotations
 
